09f31e5797d7166f124cc37b3330c125fa0786f6
TahaFazlani <tahafazlani79@gmail.com>
2025-08-17 02:57:45 +0500

    set the cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 dotenv.config();
+import cors from 'cors';
 import express from 'express';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
@@ -17,6 +18,15 @@ const app = express();
 connectDB();
 
 // Middleware
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL, 
+      "http://localhost:5173"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static("public"));
