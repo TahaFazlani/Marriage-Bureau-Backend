78d0576bc6b4e4a10ab5b0e79372cc75668a38c1
TahaFazlani <tahafazlani79@gmail.com>
2025-08-22 00:47:56 +0500

    create getCurrentUser route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,9 +5,10 @@ import {
   verifyEmail,
   resendVerification,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  getCurrentUser
 } from '../controllers/authController.js';
-
+import {auth} from '../middleware/auth.js';
 const router = express.Router();
 
 router.post('/register', register);
@@ -21,5 +22,7 @@ router.post('/resend-verification', resendVerification);
 router.post('/forgot-password', forgotPassword);
 
 router.post('/reset-password/:token', resetPassword);
+
+router.get('/me', auth, getCurrentUser);
 
 export default router;
